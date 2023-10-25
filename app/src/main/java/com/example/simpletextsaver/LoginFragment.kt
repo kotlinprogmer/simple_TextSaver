c9891ae10199276175dbbf9749b1952f232e7676
apple <kotlinprogmer@gmail.com>
2023-10-25 14:40:49 -0700

    i make activity main

--- app/src/main/java/com/example/simpletextsaver/LoginFragment.kt
+++ app/src/main/java/com/example/simpletextsaver/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.simpletextsaver.databinding.FragmentLoginBinding
 
 
@@ -23,7 +24,10 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        binding.txtRegisterPager.setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_loginFragment_to_registerFragment)
+        }
 
     }
 
